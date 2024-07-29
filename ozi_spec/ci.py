c5fbfcb46b50f8212c28ecb5d2def521179e45a9
rjdbcm <rjdbcm@outlook.com>
2024-07-28 19:26:25 -0500

    perf: publish 0.1.9
    Signed-off-by: rjdbcm <rjdbcm@outlook.com>

--- ozi_spec/ci.py
+++ ozi_spec/ci.py
@@ -17,7 +17,7 @@ class Publish(Default):
     """Publishing patterns for packaged project."""
 
     include: tuple[str, ...] = ('*.tar.gz', '*.whl', 'sig/*')
-    version: str = '0.1.8'
+    version: str = '0.1.9'
 
 
 @dataclass(slots=True, frozen=True, eq=True)
