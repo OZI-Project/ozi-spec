54718a326cffd2e8494d9dea6fc6ae6e91cdc9d2
rjdbcm <rjdbcm@outlook.com>
2024-08-22 22:57:41 -0500

    fix: pin spec to 0.8 and OZI to 1.19
    Signed-off-by: rjdbcm <rjdbcm@outlook.com>

--- ozi_spec/_spec.py
+++ ozi_spec/_spec.py
@@ -18,7 +18,7 @@ class Spec(Default):
     """OZI Specification metadata."""
 
     version: str = field(
-        default='0.6',
+        default='0.8',
         metadata={'help': 'OZI specification standard version.'},
     )
     python: PythonProject = ClassicProject()
@@ -36,7 +36,7 @@ class OZI(Default):
     """OZI distribution metadata."""
 
     version: str = field(
-        default='1.18',
+        default='1.19',
         metadata={'help': 'Currently installed version of the OZI package.'},
     )
     python_support: PythonSupport = PythonSupport()
