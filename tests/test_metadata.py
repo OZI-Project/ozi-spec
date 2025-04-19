7b3d9274631c62bf1c6612002a2dab1e80af80c5
Eden Ross Duff MSc <rjdbcm@outlook.com>
2025-04-19 17:51:41 -0500

    build: catch warning for pending deprecation python versions
    Signed-off-by: Eden Ross Duff MSc <rjdbcm@outlook.com>

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,7 +1,11 @@
 # noqa: INP001
+import sys
 
 
 def test_metadata() -> None:
-    from ozi_spec import METADATA
-
+    if sys.version_info < (3, 11):
+        with pytest.raises(FutureWarning):
+            from ozi_spec import METADATA
+    else:
+        from ozi_spec import METADATA
     METADATA.asdict()
