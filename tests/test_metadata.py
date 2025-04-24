2e8ba29991ad7d25049dbd9cbc6e371e5009dbcb
Eden Ross Duff MSc <rjdbcm@outlook.com>
2025-04-24 02:53:27 -0500

    fix: ignore FutureWarning in pre 3.11 test env
    Signed-off-by: Eden Ross Duff MSc <rjdbcm@outlook.com>

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,12 +1,12 @@
 # noqa: INP001
 import sys
-from contextlib import suppress
+import warnings
 
 
 def test_metadata() -> None:
     if sys.version_info < (3, 11):
-        with suppress(FutureWarning):
-            from ozi_spec import METADATA
+        warnings.filterwarnings('ignore')
+        from ozi_spec import METADATA
     else:
         from ozi_spec import METADATA
     METADATA.asdict()
